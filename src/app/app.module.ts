0e76ccf20915cb871580d8eab186e8ee30d4882a
Guilherme Dalmarco <dalmarco.gd@gmail.com>
2018-04-23 14:39:31 -0300

    Test ionic-super and google Plus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { Settings } from '../providers/providers';
 import { User } from '../providers/providers';
 import { Api } from '../providers/providers';
 import { MyApp } from './app.component';
+import { GooglePlus } from '@ionic-native/google-plus';
 
 // The translate loader needs to know where to load i18n files
 // in Ionic's static asset pipeline.
@@ -64,6 +65,7 @@ export function provideSettings(storage: Storage) {
     Camera,
     SplashScreen,
     StatusBar,
+    GooglePlus, 
     { provide: Settings, useFactory: provideSettings, deps: [Storage] },
     // Keep this to enable Ionic's runtime error handling during development
     { provide: ErrorHandler, useClass: IonicErrorHandler }
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { IonicPage, NavController, ToastController } from 'ionic-angular';
+import { IonicPage, NavController, ToastController, LoadingController } from 'ionic-angular';
 
 import { User } from '../../providers/providers';
 import { MainPage } from '../pages';
+import { GooglePlus } from '@ionic-native/google-plus';
 
 @IonicPage()
 @Component({
@@ -23,13 +24,35 @@ export class LoginPage {
   private loginErrorString: string;
 
   constructor(public navCtrl: NavController,
+    public loadingCtrl: LoadingController,
     public user: User,
     public toastCtrl: ToastController,
-    public translateService: TranslateService) {
+    public translateService: TranslateService,
+    public googlePlus: GooglePlus) {
 
     this.translateService.get('LOGIN_ERROR').subscribe((value) => {
       this.loginErrorString = value;
     })
+  }
+
+  doLoginGoogle() {
+    let nav = this.navCtrl;
+	let env = this;
+  let loading = this.loadingCtrl.create({
+    content: 'Please wait...'
+  });
+  loading.present();
+  this.googlePlus.login({
+    'scopes': '', // optional, space-separated list of scopes, If not included or empty, defaults to `profile` and `email`.
+    'webClientId': 'webClientId.apps.googleusercontent.com', // optional clientId of your Web application from Credentials settings of your project - On Android, this MUST be included to get an idToken. On iOS, it is not required.
+    'offline': true
+  })
+  .then(function (user) {
+    loading.dismiss();
+
+  }, function (error) {
+    loading.dismiss();
+  });
   }
 
   // Attempt to login in through our User service
